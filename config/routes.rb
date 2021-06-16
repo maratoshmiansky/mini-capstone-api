ac6ca31ef2ca0bd9ec87c3f2a5df2058f32dfc97
Marat Oshmiansky <maratical@gmail.com>
2021-06-16 11:33:16 -0700

    adds Products controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/available_products", controller: "products", action: "available_products"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+product1 = Product.create(name: "Widget", price: 13, image_url: "", description: "Typical widget thingy.")
+product2 = Product.create(name: "Foobar", price: 37, image_url: "", description: "Better than your average foobar!")
+product3 = Product.create(name: "Gadget", price: 6, image_url: "", description: "Indispensible tool for your home.")
