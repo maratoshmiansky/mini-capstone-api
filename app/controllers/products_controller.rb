acb7963f67f77f376b6d48da2efd4f16e6b65d7d
Marat Oshmiansky <maratical@gmail.com>
2021-06-16 11:40:38 -0700

    adds individual products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,17 @@
 class ProductsController < ApplicationController
-  def available_products
+  def all_products
     render json: Product.all.as_json
+  end
+
+  def product1
+    render json: Product.find_by(name: "Widget").as_json
+  end
+
+  def product2
+    render json: Product.find_by(name: "Foobar").as_json
+  end
+
+  def product3
+    render json: Product.find_by(name: "Gadget").as_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/available_products", controller: "products", action: "available_products"
+  get "/all_products", controller: "products", action: "all_products"
+  get "/product1", controller: "products", action: "product1"
+  get "/product2", controller: "products", action: "product2"
+  get "/product3", controller: "products", action: "product3"
 end
