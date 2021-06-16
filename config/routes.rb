390a4a183ef40d65d526d29d468f0c7833d92fdb
Marat Oshmiansky <maratical@gmail.com>
2021-06-16 12:16:45 -0700

    updates routes code

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/all_products", controller: "products", action: "all_products"
-  get "/product1", controller: "products", action: "product1"
-  get "/product2", controller: "products", action: "product2"
-  get "/product3", controller: "products", action: "product3"
+  # get "/all_products", controller: "products", action: "all_products"
+  # get "/product1", controller: "products", action: "product1"
+  # get "/product2", controller: "products", action: "product2"
+  # get "/product3", controller: "products", action: "product3"
+  get "/all_products" => "products#all_products"
+  get "/product1" => "products#product1"
+  get "/product2" => "products#product2"
+  get "/product3" => "products#product3"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-product1 = Product.create(name: "Widget", price: 13, image_url: "", description: "Typical widget thingy.")
-product2 = Product.create(name: "Foobar", price: 37, image_url: "", description: "Better than your average foobar!")
-product3 = Product.create(name: "Gadget", price: 6, image_url: "", description: "Indispensible tool for your home.")
+
+product1 = Product.create(name: "Widget", price: 13, image_url: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.economist.com%2Fsites%2Fdefault%2Ffiles%2Fimages%2Fprint-edition%2F20150905_TQP001_0.jpg&imgrefurl=https%3A%2F%2Fwww.economist.com%2Ftechnology-quarterly%2F2015%2F09%2F03%2Fwonderful-widgets&tbnid=sbyVTCPfTmdISM&vet=12ahUKEwjXm4DS6JzxAhVCbK0KHeWyAvkQMygHegUIARC0AQ..i&docid=4g7jwHMlOAVL7M&w=595&h=335&itg=1&q=widget&hl=en-GB&ved=2ahUKEwjXm4DS6JzxAhVCbK0KHeWyAvkQMygHegUIARC0AQ", description: "Manufactured with pride!")
+
+product2 = Product.create(name: "Thingy", price: 37, image_url: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0027%2F9953%2F4169%2Ffiles%2FImage_46_600x600.jpg%3Fv%3D1618067293&imgrefurl=https%3A%2F%2Fwww.pillowthingy.com%2F&tbnid=hXz64XdO-9PyAM&vet=12ahUKEwistpfI6ZzxAhUBT6wKHVZ5CBMQMygMegUIARD7AQ..i&docid=O3pa-Vvdyazy8M&w=600&h=400&itg=1&q=thingy&hl=en-GB&ved=2ahUKEwistpfI6ZzxAhUBT6wKHVZ5CBMQMygMegUIARD7AQ", description: "Better than your average thingy!")
+
+product3 = Product.create(name: "Gadget", price: 6, image_url: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.ytimg.com%2Fvi%2F17c6GfXVw8g%2Fmaxresdefault.jpg&imgrefurl=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D17c6GfXVw8g&tbnid=ZMAMR1T2ZQ5YfM&vet=12ahUKEwjr94XV6ZzxAhUOOawKHW0EBXYQMygKegUIARDtAQ..i&docid=vkszgUeLjNNHkM&w=1280&h=720&q=gadget&hl=en-GB&ved=2ahUKEwjr94XV6ZzxAhUOOawKHW0EBXYQMygKegUIARDtAQ", description: "Indispensible tool for your home.")
