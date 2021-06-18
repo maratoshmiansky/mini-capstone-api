31e386a855a63bf6f2a3b5c64eda9edb72bda215
Marat Oshmiansky <maratical@gmail.com>
2021-06-18 12:14:44 -0700

    add destroy route and action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,26 +1,4 @@
 class ProductsController < ApplicationController
-  # def all_products
-  #   render json: Product.all.as_json
-  # end
-
-  # def product1
-  #   render json: Product.find_by(name: "Widget").as_json
-  # end
-
-  # def product2
-  #   render json: Product.find_by(name: "Thingy").as_json
-  # end
-
-  # def product3
-  #   render json: Product.find_by(name: "Gadget").as_json
-  # end
-
-  # def any_product
-  #   input = params["name"]
-  #   product = Product.find_by(name: input)
-  #   render json: product.as_json
-  # end
-
   def index
     products = Product.all
     render json: products.as_json
@@ -51,5 +29,11 @@ class ProductsController < ApplicationController
     product.description = params["description"] || product.description
     product.save
     render json: product.as_json
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    render json: { message: "Product destroyed!" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # get "/all_products" => "products#all_products"
-  # get "/product1" => "products#product1"
-  # get "/product2" => "products#product2"
-  # get "/product3" => "products#product3"
-  # get "/any_product/#product_key" => "products#any_product"
   get "/products" => "products#index"
   get "/products/:id" => "products#show"
   post "/products" => "products#create"
   patch "/products/:id" => "products#update"
+  delete "/products/:id" => "products#destroy"
 end
