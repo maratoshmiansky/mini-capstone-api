1635cd7b4059a86642af30b6077a982b2371b7f1
Marat Oshmiansky <maratical@gmail.com>
2021-06-18 10:29:07 -0700

    add RESTful routes and actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,23 +1,34 @@
 class ProductsController < ApplicationController
-  def all_products
-    render json: Product.all.as_json
+  # def all_products
+  #   render json: Product.all.as_json
+  # end
+
+  # def product1
+  #   render json: Product.find_by(name: "Widget").as_json
+  # end
+
+  # def product2
+  #   render json: Product.find_by(name: "Thingy").as_json
+  # end
+
+  # def product3
+  #   render json: Product.find_by(name: "Gadget").as_json
+  # end
+
+  # def any_product
+  #   input = params["name"]
+  #   product = Product.find_by(name: input)
+  #   render json: product.as_json
+  # end
+
+  def index
+    products = Product.all
+    render json: products.as_json
   end
 
-  def product1
-    render json: Product.find_by(name: "Widget").as_json
-  end
-
-  def product2
-    render json: Product.find_by(name: "Thingy").as_json
-  end
-
-  def product3
-    render json: Product.find_by(name: "Gadget").as_json
-  end
-
-  def any_product
-    input = params["name"]
-    product = Product.find_by(name: input)
+  def show
+    product_id = params["id"]
+    product = Product.find(product_id)
     render json: product.as_json
   end
 end
