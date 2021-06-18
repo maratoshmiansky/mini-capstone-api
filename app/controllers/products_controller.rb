6108cf7cdfaee4aac6f2326ec4130a75c77ebbf8
Marat Oshmiansky <maratical@gmail.com>
2021-06-18 10:56:39 -0700

    add create route and action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -31,4 +31,15 @@ class ProductsController < ApplicationController
     product = Product.find(product_id)
     render json: product.as_json
   end
+
+  def create
+    product = Product.new(
+      name: params["name"],
+      price: params["price"],
+      image_url: params["image_url"],
+      description: params["description"],
+    )
+    product.save
+    render json: product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # get "/all_products", controller: "products", action: "all_products"
-  # get "/product1", controller: "products", action: "product1"
-  # get "/product2", controller: "products", action: "product2"
-  # get "/product3", controller: "products", action: "product3"
   # get "/all_products" => "products#all_products"
   # get "/product1" => "products#product1"
   # get "/product2" => "products#product2"
@@ -11,4 +7,5 @@ Rails.application.routes.draw do
   # get "/any_product/#product_key" => "products#any_product"
   get "/products" => "products#index"
   get "/products/:id" => "products#show"
+  post "/products" => "products#create"
 end
