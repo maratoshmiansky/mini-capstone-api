c14cc35a57952f166f654beb05e45a946caa6e52
Marat Oshmiansky <maratical@gmail.com>
2021-06-25 12:06:53 -0700

    add model methods and serializer attribute

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,20 +1,23 @@
 class Product < ApplicationRecord
   validates :name, uniqueness: true
   validates :name, presence: true
-  # validates :price, presence: true
   validates :price, numericality: { greater_than: 0 }  # numericality tests presence, too
   validates :description, presence: true
   # validates :active, presence: true
+
+  def supplier # allows me to do product.supplier
+    Supplier.find_by(id: supplier_id)
+  end
 
   def is_discounted?
     price < 10 # returns true if price < 10 and returns false otherwise
   end
 
   def tax
-    tax = price * 0.09
+    price * 0.09
   end
 
   def total
-    total = price + tax
+    price + tax
   end
 end
