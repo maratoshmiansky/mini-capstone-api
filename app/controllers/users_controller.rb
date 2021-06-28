a37405fc2745551b3ac263c7bbafaad6fb8cf190
Marat Oshmiansky <maratical@gmail.com>
2021-06-28 11:00:04 -0700

    add sessions controller and jwt gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  # def index
+  #   users = User.all
+  #   render json: users.as_json  # need to create a User serializer
+  # end
+
   def create
     user = User.new(
       name: params[:name],
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   patch "/products/:id" => "products#update"
   delete "/products/:id" => "products#destroy"
 
+  # get "/users" => "users#index"
   post "/users" => "users#create"
+
+  post "/sessions" => "sessions#create"
 end
