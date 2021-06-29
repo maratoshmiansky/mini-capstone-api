b4a0d258e1adaea606bef2a088831826cd43406e
Marat Oshmiansky <maratical@gmail.com>
2021-06-29 10:53:52 -0700

    add index and show routes and controller actions for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,14 @@
 class OrdersController < ApplicationController
+  def index
+    orders = Order.all
+    render json: orders
+  end
+
+  def show
+    order = Order.find(params[:id])
+    render json: order
+  end
+
   def create
     order = Order.new(
       user_id: params[:user_id],
