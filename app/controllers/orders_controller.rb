70e15cbacd2c88188f00bbcdd9b2ac0f6e88d407
Marat Oshmiansky <maratical@gmail.com>
2021-06-29 12:19:48 -0700

    update orders controller create action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,13 +10,17 @@ class OrdersController < ApplicationController
   end
 
   def create
+    product = Product.find_by(id: params[:product_id])
+    calculated_subtotal = product.price * params[:quantity].to_i
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
     order = Order.new(
-      user_id: params[:user_id],
+      user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
-      subtotal: params[:subtotal],
-      tax: params[:tax],
-      total: params[:total],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total,
     )
     if order.save
       render json: { message: "Order created successfully" }, status: :created
