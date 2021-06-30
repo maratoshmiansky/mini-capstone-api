0ee631fd551c35cea2c2375fafb0002146d9cdb7
Marat Oshmiansky <maratical@gmail.com>
2021-06-30 11:06:03 -0700

    add user and admin authentication to products and orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,40 +1,30 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user
+
   def index
-    if current_user
-      # orders = Order.all
-      orders = current_user.orders.all
-      render json: orders
-    else
-      render json: [], status: :unauthorized
-    end
+    # orders = Order.all
+    orders = current_user.orders.all
+    render json: orders
   end
 
   def show
-    if current_user
-      order = current_user.orders.find_by(id: params[:id])  # only want to see the current logged in user's order
-      render json: order
-    else
-      render json: [], status: :unauthorized
-    end
+    order = current_user.orders.find_by(id: params[:id])  # only want to see the current logged in user's order
+    render json: order
   end
 
   def create
-    if current_user
-      product = Product.find_by(id: params[:product_id])
-      calculated_subtotal = product.price * params[:quantity].to_i
-      calculated_tax = calculated_subtotal * 0.09
-      calculated_total = calculated_subtotal + calculated_tax
-      order = Order.new(
-        user_id: current_user.id,
-        product_id: params[:product_id],
-        quantity: params[:quantity],
-        subtotal: calculated_subtotal,
-        tax: calculated_tax,
-        total: calculated_total,
-      )
-    else
-      render json: [], status: :unauthorized
-    end
+    product = Product.find_by(id: params[:product_id])
+    calculated_subtotal = product.price * params[:quantity].to_i
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+    order = Order.new(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total,
+    )
     if order.save
       render json: { message: "Order created successfully" }, status: :created
     else
