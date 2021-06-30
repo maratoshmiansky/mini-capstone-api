161356a0a854e16f1054d5d259beb94859aa8bf2
Marat Oshmiansky <maratical@gmail.com>
2021-06-30 09:58:12 -0700

    add authorization to orders controller actions

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,12 @@
 class OrdersController < ApplicationController
   def index
-    orders = Order.all
-    render json: orders
+    if current_user
+      # orders = Order.all
+      orders = current_user.orders.all
+      render json: orders
+    else
+      render json: [], status: :unauthorized
+    end
   end
 
   def show
@@ -9,23 +14,27 @@ class OrdersController < ApplicationController
       order = current_user.orders.find_by(id: params[:id])  # only want to see the current logged in user's order
       render json: order
     else
-      render json: []  # change this to show a not logged in error message
+      render json: [], status: :unauthorized
     end
   end
 
   def create
-    product = Product.find_by(id: params[:product_id])
-    calculated_subtotal = product.price * params[:quantity].to_i
-    calculated_tax = calculated_subtotal * 0.09
-    calculated_total = calculated_subtotal + calculated_tax
-    order = Order.new(
-      user_id: current_user.id,
-      product_id: params[:product_id],
-      quantity: params[:quantity],
-      subtotal: calculated_subtotal,
-      tax: calculated_tax,
-      total: calculated_total,
-    )
+    if current_user
+      product = Product.find_by(id: params[:product_id])
+      calculated_subtotal = product.price * params[:quantity].to_i
+      calculated_tax = calculated_subtotal * 0.09
+      calculated_total = calculated_subtotal + calculated_tax
+      order = Order.new(
+        user_id: current_user.id,
+        product_id: params[:product_id],
+        quantity: params[:quantity],
+        subtotal: calculated_subtotal,
+        tax: calculated_tax,
+        total: calculated_total,
+      )
+    else
+      render json: [], status: :unauthorized
+    end
     if order.save
       render json: { message: "Order created successfully" }, status: :created
     else
