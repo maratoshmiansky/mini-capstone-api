e2041b10506f015bd5f17f4d51dfbf22a783f05a
Marat Oshmiansky <maratical@gmail.com>
2021-07-02 05:54:18 -0700

    update products controller - strings to symbols and fixed happy/sad paths

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,32 +14,32 @@ class ProductsController < ApplicationController
 
   def create
     product = Product.new(
-      name: params["name"],
-      price: params["price"],
-      image_url: params["image_url"],
-      description: params["description"],
-      stock: params["stock"],
-      supplier_id: params["supplier_id"],
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description],
+      stock: params[:stock],
+      supplier_id: params[:supplier_id],
     )
     if product.save
-      render json: product
+      render json: product, status: :created
     else
-      render json: { errors: product.errors.full_messages }, status: 418  # teapot
+      render json: { errors: product.errors.full_messages }, status: :unauthorized
     end
   end
 
   def update
     product = Product.find(params[:id])
-    product.name = params["name"] || product.name
-    product.price = params["price"] || product.price
-    product.image_url = params["image_url"] || product.image_url
-    product.description = params["description"] || product.description
-    product.stock = params["stock"] || product.stock
-    product.supplier_id = params["supplier_id"] || product.supplier_id
+    product.name = params[:name] || product.name
+    product.price = params[:price] || product.price
+    product.image_url = params[:image_url] || product.image_url
+    product.description = params[:description] || product.description
+    product.stock = params[:stock] || product.stock
+    product.supplier_id = params[:supplier_id] || product.supplier_id
     if product.save
-      render json: product
+      render json: product, status: :created
     else
-      render json: { errors: product.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: product.errors.full_messages }, status: :unauthorized
     end
   end
 
