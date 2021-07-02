152c3c7eea3ab63b1ddf81cfd58138c013e768a1
Marat Oshmiansky <maratical@gmail.com>
2021-07-02 10:51:21 -0700

    add migration: ChangePricePrecision (from 4 to 9)

---
+++ db/migrate/20210702174814_change_price_precision.rb
@@ -0,0 +1,5 @@
+class ChangePricePrecision < ActiveRecord::Migration[6.1]
+  def change
+    change_column :products, :price, :decimal, precision: 9, scale: 2
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_02_164505) do
+ActiveRecord::Schema.define(version: 2021_07_02_174814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2021_07_02_164505) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.decimal "price", precision: 4, scale: 2
+    t.decimal "price", precision: 9, scale: 2
     t.string "image_url"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
